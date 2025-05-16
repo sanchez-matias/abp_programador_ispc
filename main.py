e6d7cc448c9d02c1b1ead83955516a95c507d728
sanchez-matias <matias.emanuel.sanchez@hotmail.com>
2025-05-16 19:21:56 -0300

    Creación de los dispositivos e inicio del proyecto

---
+++ main.py
@@ -0,0 +1,44 @@
+
+def agregar_dispositivo():
+    pass
+
+def listar_dispositivos():
+    pass
+
+def buscar_dispositivo():
+    pass
+
+def eliminar_dispositivo():
+    pass
+
+dispositivos = {}
+
+def menu():
+    while True:
+        print("\n--- MENÚ PRINCIPAL ---")
+        print("1. Agregar dispositivo")
+        print("2. Listar dispositivos")
+        print("3. Buscar dispositivo")
+        print("4. Eliminar dispositivo")
+        print("5. Activar Modo Fiesta")
+        print("6. Activar Modo Noche")
+        print("7. Salir")
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            agregar_dispositivo()
+        elif opcion == "2":
+            listar_dispositivos()
+        elif opcion == "3":
+            buscar_dispositivo()
+        elif opcion == "4":
+            eliminar_dispositivo()
+        elif opcion == "5":
+            activar_modo_fiesta(dispositivos)
+        elif opcion == "6":
+            activar_modo_noche(dispositivos)
+        elif opcion == "7":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción inválida. Intente de nuevo.")
